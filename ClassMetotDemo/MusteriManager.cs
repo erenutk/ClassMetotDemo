65fcc6404381ee1d4f7b375ac4b90bac75e55cb4
erenutk <erenutkukrts@gmail.com>
2021-01-17 16:46:57 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        List<Musteri> musteriListesi = new List<Musteri>();
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi +" " + musteri.Soyadi + " " + "listeye eklendi.");
+            musteriListesi.Add(musteri);
+        }
+        public void Listele()
+        {
+            Console.WriteLine("Liste:");
+            foreach (var x in musteriListesi)
+            {
+                Console.WriteLine(+ x.Id + " " + x.Adi+ " " + x.Soyadi);
+            }
+            if (musteriListesi.Count == 0)               
+            {
+                Console.WriteLine("Liste boş.");
+            }
+            
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi+ " " + "listeden çıkarıldı.");
+            musteriListesi.Remove(musteri);
+        }
+    }
+}
+
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri müsteri1 = new Musteri()
+            {
+                Id = 1,
+                Adi = "Erkan",
+                Soyadi = "Coşar",
+            };
+            Musteri müsteri2 = new Musteri()
+            {
+                Id = 2,
+                Adi = "Burhan",
+                Soyadi = "Bülbül",
+            };
+            Musteri müsteri3 = new Musteri()
+            {
+                Id = 3,
+                Adi = "Ömür",
+                Soyadi = "Okumuş",
+            };
+
+            MusteriManager müsteriManager = new MusteriManager();
+            müsteriManager.Ekle(müsteri1);
+            müsteriManager.Ekle(müsteri2);
+            müsteriManager.Ekle(müsteri3);
+
+            Console.WriteLine("");
+
+            müsteriManager.Listele();
+
+            Console.WriteLine("");
+
+            müsteriManager.Sil(müsteri1);
+            müsteriManager.Sil(müsteri2);
+            müsteriManager.Sil(müsteri3);
+
+            Console.WriteLine("");
+
+            müsteriManager.Listele();
+
+
+        }
+    }
+}
